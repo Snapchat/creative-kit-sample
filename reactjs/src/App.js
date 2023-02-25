86dbaa368d20116552990f4b4dc4bb779de5084f
cr34t1ve <mrsofua@gmail.com>
2023-02-25 14:51:35 +0000

    react js sample Reactjs Creative Kit Sample

---
+++ reactjs/src/App.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div
+          className="snapchat-creative-kit-share"
+          data-share-url="https://www.ggoogle.com/"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
